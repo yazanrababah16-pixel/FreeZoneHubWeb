eb7e6f777fd369dca612d69c186565015306d0d1
yazanrababah16-pixel <yazanrababah16@gmail.com>
2026-05-17 01:01:10 +0300

    fix: vite config to match base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "./",
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  base: '/FreeZoneHubWeb/',
 })
